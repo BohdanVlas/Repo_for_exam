37723794ffa9a5d241b4d4253c7633c64bc6cca3
BohdanVlas <bodik.vlasenko@gmail.com>
2024-01-17 12:55:43 +0200

    V1.1

--- exam_test.py
+++ exam_test.py
@@ -4,11 +4,11 @@ import exam_modules as test_app
 class TestVidssum(unittest.TestCase):
     
     def test_values(self):
-        self.assertRaises(ValueError, vidssum, -10000, 6, 17)
+        self.assertRaises(ValueError, test_app.vidssum, -10000, 6, 17)
 
     def test_values(self):
-        self.assertFalse(TypeError, vidssum, 10000, 6, 'O')
-        self.assertFalse(TypeError, vidssum, 10000, 6, True)
+        self.assertFalse(TypeError, test_app.vidssum, 10000, 6, 'O')
+        self.assertFalse(TypeError, test_app.vidssum, 10000, 6, True)
 
 if __name__ == '__main__':
     unittest.main()
